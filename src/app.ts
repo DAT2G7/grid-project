5d25e84c1f476bf3be5e5bd0116140503c50e8b3
HPT-I <isak@hauptmann.tech>
2023-05-22 11:41:38 +0200

    asdf

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,5 @@
-import { GetTaskQuery, RawData } from "./interfaces";
-import { createJob, registerJob } from "./task.model";
 import config from "./config";
 import express from "express";
-import bodyParser from "body-parser";
 import { runSetup } from "./setup";
 import { DatabaseHandler } from "./db";
 
@@ -11,9 +8,3 @@ const app = express();
 const port = config.PORT;
 const database = DatabaseHandler.getInstance();
 runSetup();
-
-// Start server
-app.listen(port, () => {
-    
-    //console.log("Server started on port", port);
-});
