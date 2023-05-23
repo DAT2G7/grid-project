aacc3987c70dfbe7247e2e4fde9b734f7e5a5512
HPT-I <isak@hauptmann.tech>
2023-05-23 09:31:50 +0200

    asd

--- src/setup.ts
+++ src/setup.ts
@@ -19,18 +19,50 @@ function calculateStats() {
         for (let j = 0; j < job.tasks.length; j++) {
             const task = job.tasks[j];
             if (task.delegationTime && task.completionTime) {
-
                 const startDate = Date.parse(task.delegationTime.toString());
                 const endDate = Date.parse(task.completionTime.toString());
                 taskCompletionTimes.push(endDate - startDate);
             }
         }
     }
-    
-    const totalTaskCompletionTime = taskCompletionTimes.reduce((a, b) => a + b, 0);
-    const averageTaskCompletionTime = totalTaskCompletionTime / taskCompletionTimes.length;
 
-    console.log("Average task completion time: " + averageTaskCompletionTime / 1000 + " seconds");
+    const totalTaskCompletionTime = taskCompletionTimes.reduce(
+        (a, b) => a + b,
+        0
+    );
+    const averageTaskCompletionTime =
+        totalTaskCompletionTime / taskCompletionTimes.length;
 
+    console.log(
+        "Total task completion time: " +
+            totalTaskCompletionTime / 1000 +
+            " seconds"
+    );
 
+    taskCompletionTimes.sort((a, b) => a - b);
+    if (totalTaskCompletionTime % 2 == 0) {
+        const medianTaskCompletionTime =
+            (taskCompletionTimes[taskCompletionTimes.length / 2] +
+                taskCompletionTimes[taskCompletionTimes.length / 2 - 1]) /
+            2;
+        console.log(
+            "Median task completion time: " +
+                medianTaskCompletionTime / 1000 +
+                " seconds"
+        );
+    } else {
+        const medianTaskCompletionTime =
+            taskCompletionTimes[Math.floor(taskCompletionTimes.length / 2)];
+        console.log(
+            "Median task completion time: " +
+                medianTaskCompletionTime / 1000 +
+                " seconds"
+        );
+    }
+
+    console.log(
+        "Average task completion time: " +
+            averageTaskCompletionTime / 1000 +
+            " seconds"
+    );
 }
